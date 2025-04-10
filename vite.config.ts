d4c3b42ebd4267f302a614dbccd4101315857a2b
Jan Ackermann <jan.ackermann@real-digital.de>
2025-04-10 22:01:27 +0200

    feat: enhance PWA support with updated manifest and improved HTML structure

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,50 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.ico',
+        'app-icon.png',
+        'app-icon_x96.png',
+        'app-icon_x128.png',
+        'app-icon_x192.png',
+      ],
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'app-icon_x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'app-icon_x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'pwa-icon_x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'app-icon_x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
